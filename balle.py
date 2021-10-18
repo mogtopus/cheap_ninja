ab8022e17a2056ee42e0f44c90bb280563eda778
mogtopus <ugo.vogo@gmail.com>
2021-10-18 21:36:25 +0400

    squelette du code

---
+++ balle.py
@@ -0,0 +1,29 @@
+class Balle:
+    def __init__(self , x , y , vx , vy , diam , r , g , b):
+        self.x = self.x
+        self.y = self.y
+        self.vx = self.vx
+        self.vy = self.vy
+        self.diam = diam
+        self.r = r
+        self.g = g
+        self.b = b
+        
+    def gravity(self): #pauline
+        pass
+        
+    def sinus(self):
+        pass
+    
+    def bounce(self):
+        pass
+    
+    def anime(self):
+        pass
+    
+    def est_dans(self):
+        pass
+        
+    def affiche(self):
+        fill(self.r self.g , self.b)
+        circle(self.x , self.y , self.diam)
---
+++ cheap_ninja.pyde
@@ -0,0 +1,7 @@
+from balle import *
+
+def setup():
+    size(500,500)
+    frameRate(60)
+def draw():
+    
