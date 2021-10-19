db75770b6b98ae6b95fe700a51b586edf648c59f
mogtopus <ugo.vogo@gmail.com>
2021-10-19 15:32:48 +0400

    bouncy

--- balle.py
+++ balle.py
@@ -1,5 +1,5 @@
 class Balle:
-    XMAX = 500
+    XMAX = 750
     YMAX = 500
     def __init__(self , x , y , vx , vy , diam , r , g , b):
         self.x = x
@@ -10,6 +10,7 @@ class Balle:
         self.r = r
         self.g = g
         self.b = b
+        self.ray = diam/2
         
     def gravity(self):
         pass
@@ -18,8 +19,22 @@ class Balle:
         pass
     
     def bounce(self):
-        #if self.x 
-        pass
+        if self.x - self.ray <= 0:
+            self.x = 0 + self.ray
+            self.vx = -self.vx
+        elif Balle.XMAX <= self.x + self.ray:
+            self.x = Balle.XMAX - self.ray
+            self.vx = -self.vx
+
+        if self.y - self.ray <= 0 :
+            self.y = 0 + self.ray
+            self.vy = -self.vy
+        elif Balle.YMAX <= self.y + self.ray:
+            self.y = Balle.YMAX -self.ray
+            self.vy = -self.vy
+        
+        self.deplace()
+        self.affiche()
     
     def anime(self):
         pass
--- cheap_ninja.pyde
+++ cheap_ninja.pyde
@@ -3,5 +3,9 @@ from balle import *
 def setup():
     size(Balle.XMAX,Balle.YMAX)
     frameRate(60)
-def draw():
+    background(0,0,0)
+test = Balle(250,250 , 10,10 , 20 , 100,150,200)
     
+def draw():
+    global test
+    test.bounce()
